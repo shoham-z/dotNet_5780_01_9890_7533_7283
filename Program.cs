fc771856aed4ee87f69bf2db19b44d08af3077a7
shoham-z <shozeh123@gmail.com>
2019-12-15 21:02:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNet_5780_01_9890_7533_7283
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
